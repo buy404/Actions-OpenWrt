ed1e0c23365e6413f1259784b9df4c93943a6709
buy404 <99722055+buy404@users.noreply.github.com>
2024-11-24 22:16:08 +0800

    Update immortalwrt_openwrt.sh

--- immortalwrt_openwrt.sh
+++ immortalwrt_openwrt.sh
@@ -257,11 +257,10 @@ TOOLS_HASH=$(git log --pretty=tformat:"%h" -n1 tools toolchain)
 CACHE_NAME="$SOURCE_NAME-${REPO_BRANCH#*-}-$NAME-cache-$TOOLS_HASH"
 echo "CACHE_NAME=$CACHE_NAME" >>$GITHUB_ENV
 CACHE_URL=$(curl -sL api.github.com/repos/$GITHUB_REPOSITORY/releases | awk -F '"' '/download_url/{print $4}' | grep $CACHE_NAME)
-curl -sL api.github.com/repos/$GITHUB_REPOSITORY/releases | grep -oP 'download_url": "\K[^"]*cache[^"]*' >cache_url
 
-if (grep -q "$CACHE_NAME" cache_url); then
+if [ -n $CACHE_URL ]; then
     STEP_NAME='下载toolchain'; BEGIN_TIME=$(date '+%H:%M:%S')
-    wget -qc -t=3 $(grep "$CACHE_NAME" cache_url)
+    wget -qc -t=3 $CACHE_URL
     [ -e *.tzst ]; status
     [ -e *.tzst ] && {
         STEP_NAME='部署toolchain'; BEGIN_TIME=$(date '+%H:%M:%S')
